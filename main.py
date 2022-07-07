ad602a04166801f60cb549f34638e337fb940c37
Grégory Ployart <gregory.ployartwork@gmail.com>
2022-07-07 15:14:00 +0200

    New ui added

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import sys
 from PyQt5 import QtWidgets
-
+from src.app.UI.UIMainWindow.UIMainWindow import UIMainWindow
 
 def main():
-    """
     app = QtWidgets.QApplication(sys.argv)
     window = QtWidgets.QMainWindow()
-    ui = UiMainWindow()
+    ui = UIMainWindow()
     ui.setupUi(window)
     window.show()
     sys.exit(app.exec_())
-    """
 
 
 
---
+++ src/app/_Service/AutoClickerService.py
@@ -0,0 +1,4 @@
+from pynput import keyboard
+
+class AutoClickerService:
+
