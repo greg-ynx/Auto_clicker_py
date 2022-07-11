610995905137f14e387b2465e45e4dab3e17fe05
Grégory Ployart <gregory.ployartwork@gmail.com>
2022-07-11 22:07:45 +0200

    New GUI logic

--- src/app/_Service/ParametersService.py
+++ src/app/_Service/ParametersService.py
@@ -1,12 +1,15 @@
 class ParametersService:
 
-    def __init__(self, ac):
-        self.click_interval = ac.interval_in_seconds()
-        self.mouse_button = ac.mouse_button
-        self.click_type = ac.click_type
-        self.repeat_until_stopped = ac.repeat_until_stopped
-        self.cursor_position = ac.cursor_position
+    def __init__(self, interval=0.1, mouse_button="left", click_type="Single", repeat_until_stopped=True, times=1,
+                 cursor_position=True, cursor_X=0, cursor_Y=0):
+        self.click_interval = interval
+        self.mouse_button = mouse_button
+        self.click_type = click_type
+        self.repeat_until_stopped = repeat_until_stopped
+        if not repeat_until_stopped:
+            self.times = times
+        self.cursor_position = cursor_position
         if not self.cursor_position:
-            self.cursor_X = ac.cursor_X
-            self.cursor_Y = ac.cursor_Y
+            self.cursor_X = cursor_X
+            self.cursor_Y = cursor_Y
         
